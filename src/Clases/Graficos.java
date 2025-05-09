ebcf2e71e5cb2c7b0b5d8ecc24ebc57ba1e0c33d
Alfredo Nader <anader@anahuac.mx>
2025-05-09 13:50:23 -0600

    Avances en Graficos.java. Falta dibujar la leyenda, los ticks, el método add y completar los constructores y el método (sobreescrito) paintComponent.

--- src/Clases/Graficos.java
+++ src/Clases/Graficos.java
@@ -2,6 +2,10 @@ package Clases;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import javax.swing.JPanel;
 
 /**
@@ -9,12 +13,13 @@ import javax.swing.JPanel;
  * contorno básico del gráfico, los ejes, todos los títulos y la leyenda (que
  * es opcional). Ya que se dibuja el contorno del gráfico, se le agrega una 
  * gráfica específica que se debe crear con una clase derivada. 
+ * 
  * @author Victoria Darina Basurto
  * @author Alejandro Bermudez
  * @author Alfredo Nader
  */
 
-public class Graficos {
+public class Graficos extends JPanel {
     // Atributos
     
     /** 
@@ -23,15 +28,38 @@ public class Graficos {
      */
     private final Color COLOR = new Color(0, 0, 0);
     
-    /** 
-     * La dimensión de anchura del gráfico. No se permiten valores negativos.
+    /**
+     * Font principal (usado en los ejes y ticks).
      */
-    public int width;
+    private final Font FONT = new Font("Monospaced", Font.PLAIN, 12);
+    
+    /**
+     * Margen superior de la gráfica. El contorno básico de la gráfica se va
+     * a dibujar en ({@code MARGIN_L}, {@code MARGIN_T}).
+     */
+    public final int MARGIN_T = 50;
+    
+    /**
+     * Margen izquierdo de la gráfica. El contorno básico de la gráfica se va
+     * a dibujar en ({@code MARGIN_L}, {@code MARGIN_T}).
+     */
+    public final int MARGIN_L = 30;
     
     /** 
-     * La dimensión de altura del gráfico. No se permiten valores negativos.
+     * La dimensión de anchura (width) del gráfico. No se permiten valores negativos.
      */
-    public int height;
+    public int ANCHO = 150; // WIDTH
+    
+    /** 
+     * La dimensión de altura (height) del gráfico. No se permiten valores negativos.
+     */
+    public int ALTO = 100; // HEIGHT
+    
+    /** 
+     * Nombres de columnas de datos. Útil para la layenda y para los títulos
+     * de los ejes.
+     */
+    public String[] columnas;
     
     /** 
      * Título del gráfico.
@@ -58,18 +86,6 @@ public class Graficos {
      */
     public int tickY;
     
-    /** 
-     * Nombres de columnas de datos. Útil para la layenda y para los títulos
-     * de los ejes.
-     */
-    public String[] columnas;
-    
-    /**
-     * Margen superior de la gráfica. El contorno básico de la gráfica se va
-     * a dibujar en (0, {@code MARGIN_T}).
-     */
-    public int MARGIN_T = 50;
-    
     
     // Constructores
     
@@ -77,17 +93,8 @@ public class Graficos {
      * Método constructor; TODO
      */
     public Graficos() {
-        setSize();
-        // TODO logic
-    }
-    
-    /**
-     * Método constructor; TODO
-     * @param width Ancho del gráfico
-     * @param height Altura del gráfico
-     */
-    public Graficos(int width, int height) {
-        setSize(width, height);
+        super();
+        setSize(ANCHO, ALTO);
         // TODO logic
     }
     
@@ -95,61 +102,86 @@ public class Graficos {
     // Métodos
     
     /**
-     * Establece el tamaño del gráfico en 100 x 100 pixeles.
+     * Establece la dimensión del gráfico con el objeto de {@code Dimension} 
+     * proporcionado.
+     * 
+     * @param size Dimensión del gráfico.
      */
-    public void setSize() {
-        width = height = 100;
+    @Override
+    public final void setSize(Dimension size) {
+        ANCHO = size.width;
+        ALTO = size.height;
+        setPreferredSize(getDimension());
     }
     
     /**
-     * Establece el tamaño del gráfico con los valores proporcionados.
+     * Establece el tamaño del gráfico con los valores proporcionados.<br>
+     * <strong>Nota:</strong> Los métodos {@code setSize} y {@code getSizeG} 
+     * tienen una {@code G} (de {@code Graficos}) al final para diferenciarse 
+     * de los métodos de {@code JPanel}.
+     * 
      * @param width Anchura del gráfico.
      * @param height Altura del gráfico.
      */
-    public void setSize(int width, int height) {
-        this.width = width;
-        this.height = height;
+    @Override
+    public final void setSize(int width, int height) {
+        setSize(new Dimension(MARGIN_L + width, MARGIN_T + height));
     }
     
     /**
-     * Establece la dimensión del gráfico con el objeto de {@code Dimension} 
-     * proporcionado.
-     * @param size Dimensión del gráfico.
+     * Establece el tamaño del gráfico con los valores predeterminados.
      */
-    public void setSize(Dimension size) {
-        width = size.width;
-        height = size.height;
+    public final void setSize() {
+        setSize(getDimension());
     }
     
     /**
-     * Devuelve el tamaño del gráfico como arreglo de enteros.
+     * Devuelve el tamaño del gráfico como <em>arreglo</em> de enteros.
+     * 
      * @return Tamaño del gráfico como arreglo de enteros.
      */
-    public int[] getSize() {
-        return new int[] {width, height};
+    public int[] getSizeArray() {
+        return new int[] {ANCHO, ALTO};
     }
     
     /**
-     * Devuelve el tamaño del gráfico como objeto de {@code Dimension}.
+     * Devuelve el tamaño del <em>panel</em> como instancia de {@code Dimension}.
+     * 
      * @return Tamaño del gráfico como objeto de {@code Dimension}.
      */
     public Dimension getDimension() {
-        return new Dimension(width, height);
+        return new Dimension(MARGIN_L + ANCHO, MARGIN_T + ALTO);
     }
     
     /**
      * Dibuja el contorno básico del gráfico. Éste se posiciona por default en 
-     * la posición (0, {@code MARGIN_T}) y tiene el tamaño de ({@code width}, 
-     * {@code height}). Se dibuja en esta posición vertical porque este es el 
-     * tamaño necesario para dibujar el título del gráfico y dejar un margen adecuado.
+     * la posición (0, {@code MARGIN_T}) y tiene el tamaño de ({@code ANCHO}, 
+     * {@code ALTO}). Se dibuja en esta posición vertical porque este es el 
+     * tamaño necesario para dibujar el título del gráfico y dejar un margen 
+     * adecuado.
+     * 
+     * @param g El objeto {@code Graphics} para realizar las operaciones de 
+     * dibujo en este componente.
      */
-    public void drawSize() {
-        // TODO logic
-        // Es literalmente dibujar dos líneas verticales y dos horizontales. 
+    public void drawSize(Graphics g) {
+        System.out.println("Dibujando contorno del gráfico...");
+        if (g != null) {
+            int xs = MARGIN_L;      // X Start
+            int xe = xs + ANCHO;    // X End
+            int ys = MARGIN_T;      // X Start
+            int ye = ys + ALTO;     // X End
+            
+            g.setColor(COLOR);
+            g.drawLine(xs, ys, xe, ye); // Línea de arriba
+            g.drawLine(xe, ys, xe, ye); // Línea de abajo
+            g.drawLine(xs, ys, xs, ye); // Línea de izquierda
+            g.drawLine(xe, ys, xe, ye); // Línea de derecha
+        } else NG();
     }
     
     /**
      * Establece el título del gráfico.
+     * 
      * @param t Texto del título del gráfico.
      */
     public void setTitle(String t) {
@@ -158,13 +190,23 @@ public class Graficos {
     
     /**
      * Dibuja el título del gráfico.
+     * 
+     * @param g El objeto {@code Graphics} para realizar las operaciones de 
+     * dibujo en este componente.
      */
-    public void drawTitle() {
-        // TODO logic
+    public void drawTitle(Graphics g) {
+        System.out.println("Dibujando título del gráfico...");
+        if (g != null) {
+            g.setColor(COLOR);
+            g.setFont(FONT);
+            g.drawString(titulo, MARGIN_L + (ANCHO - titulo.length()) / 2, 
+                    (MARGIN_T - 12) / 2);
+        } else NG();
     }
     
     /**
      * Establece los títulos de los ejes horizontales.
+     * 
      * @param e Texto del título del eje X.
      */
     public void setEjeX(String e) {
@@ -173,6 +215,7 @@ public class Graficos {
     
     /**
      * Establece los títulos de los ejes verticales.
+     * 
      * @param e Texto del título del eje Y.
      */
     public void setEjeY(String e) {
@@ -181,6 +224,7 @@ public class Graficos {
     
     /**
      * Establece los títulos de los ejes horizontales y verticales.
+     * 
      * @param x Texto del título del eje X.
      * @param y Texto del título del eje Y.
      */
@@ -191,6 +235,7 @@ public class Graficos {
     
     /**
      * Establece los títulos de los ejes horizontales y verticales.
+     * 
      * @param e Arreglo de los textos de los títulos de los ejes.
      */
     public void setEjes(String[] e) {
@@ -203,14 +248,39 @@ public class Graficos {
     }
     
     /**
-     * Dibuja los títulos de los ejes horizontales y vertivales.
+     * Dibuja los títulos de los ejes horizontales y verticales.
+     * 
+     * @param g El objeto {@code Graphics} para realizar las operaciones de 
+     * dibujo en este componente.
      */
-    public void drawEjes() {
-        // TODO logic
+    public void drawEjes(Graphics g) {
+        System.out.println("Dibujando títulos de ejes...");
+        if (g != null) {
+            int posX, posY;
+            
+            g.setColor(COLOR);
+            g.setFont(FONT);
+            
+            // Eje X
+            posX = MARGIN_L + (ANCHO - ejeX.length()) / 2;
+            posY = (MARGIN_T - 12) / 2;
+            g.drawString(ejeY, posX, posY);
+            
+            // Eje y
+            Graphics2D g2d = (Graphics2D) g;
+            AffineTransform t = g2d.getTransform();
+            posX = (MARGIN_L - 12) / 2;
+            posY = MARGIN_T + (ALTO - ejeY.length()) / 2;
+
+            g2d.rotate(Math.toRadians(90), posX, posY); // Rotación de 90°
+            g2d.drawString(ejeY, posX, posY);
+            g2d.setTransform(t);
+        } else NG();
     }
     
     /**
      * Establece el espacio entre los ticks horizontales.
+     * 
      * @param x Cantidad de espacio entre ticks del eje X (palitos en los ejes).
      */
     public void setTickX(int x) {
@@ -219,6 +289,7 @@ public class Graficos {
     
     /**
      * Establece el espacio entre los ticks verticales.
+     * 
      * @param y Cantidad de espacio entre ticks del eje Y (palitos en los ejes).
      */
     public void setTickY(int y) {
@@ -227,6 +298,7 @@ public class Graficos {
     
     /**
      * Establece el espacio entre los ticks horizontales y verticales.
+     * 
      * @param t Arreglo con la cantidad de espacio entre los ticks de ambos ejes.
      */
     public void setTicks(int[] t) {
@@ -240,17 +312,21 @@ public class Graficos {
     
     /**
      * Dibuja los ticks en el gráfico.
+     * 
+     * @param g El objeto {@code Graphics} para realizar las operaciones de 
+     * dibujo en este componente.
      */
-    public void drawTicks() {
+    public void drawTicks(Graphics g) {
         // TODO logic
     }
     
     /**
      * Agrega el contenido visual al gráfico en la posición proporcionada.
      * Éste debe ser creado en una clase derivada.
+     * 
      * @param posX Posición de X del contenido visual.
      * @param posY Posición de Y del contenido visual.
-     * @param vista JPanel que se agregará al gráfico. 
+     * @param vista {@code JPanel} que se agregará al gráfico. 
      */
     public void add(int posX, int posY, JPanel vista) {
         // TODO logic
@@ -258,30 +334,64 @@ public class Graficos {
     
     /**
      * Agrega (dibuja) una leyenda en la parte superior derecha.
+     * 
+     * @param g El objeto {@code Graphics} para realizar las operaciones de 
+     * dibujo en este componente.
      */
-    public void leyenda() {
+    public void leyenda(Graphics g) {
         // TODO logic
     }
     
     /**
      * Agrega (dibuja) una leyenda en una posición especificada.
+     * 
+     * @param g El objeto {@code Graphics} para realizar las operaciones de 
+     * dibujo en este componente.
      * @param posX Posición de X de la leyenda. 
      * @param posY Posición de Y de la leyenda. 
      */
-    public void leyenda(int posX, int posY) {
+    public void leyenda(Graphics g, int posX, int posY) {
         // TODO logic
     }
     
     /**
      * Agrega (dibuja) una leyenda en una posición especificada. 
+     * 
+     * @param g El objeto {@code Graphics} para realizar las operaciones de 
+     * dibujo en este componente.
      * @param pos Arreglo de posiciones de ambos ejes de la leyenda. El arreglo
      * debe contener un arreglo con exactamente dos elementos.
      */
-    public void leyenda(int[] pos) {
+    public void leyenda(Graphics g, int[] pos) {
         if (pos.length == 2) {
             // TODO logic
         } else {
             System.err.println("Error: Se tienen que proporcionar dos argumentos en leyenda(int[] pos).");
         }
+    }
+    
+    /**
+     * Método general para llamar en los métodos que dibujan cuando
+     * {@code Graphics g = null}.
+     */
+    private void NG() {
+        System.err.println("Error: No se encontró el objeto de Graphics.");
+    }
+    
+    /**
+     * Sobreescribe el método {@code paintComponent}. Este método es llamado por
+     * el sistema de pintura de Swing cada vez que el componente necesita ser 
+     * redibujado. Aquí se deben realizar todas las operaciones de dibujo para 
+     * mostrar el gráfico.
+     * 
+     * @param g El objeto {@code Graphics} para realizar las operaciones de 
+     * dibujo en este componente.
+     */
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawSize(g);
+        drawTitle(g);
+        // ...
     }
 }
